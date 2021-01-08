a00ee14f9f975aa53dfc752ad7da94f2a5a02df4
NievesBorrero <nievesborrerobarea@gmail.com>
2021-01-08 20:14:41 +0100

    fist refactor

--- Gilded-rose-kata/test/golden-master-text-test.ts
+++ Gilded-rose-kata/test/golden-master-text-test.ts
@@ -1,4 +1,5 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+import { GildedRose } from '../app/gilded-rose';
+import { Item } from '../app/item';
 
 const items = [
     new Item("+5 Dexterity Vest", 10, 20), //
